b2db388e740e8f8daff9bac9937ad37d984df5cd
Varunp01 <152736714+Varunp01@users.noreply.github.com>
2024-08-20 12:33:09 +0530

    Update Trending.jsx

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -4,15 +4,13 @@ export default function Trending(){
         <>
         <div className='Title'><h1 className='heading1'>Top Sellers</h1></div><br></br>
             <div className="TrendingContainer">
-                <div className="TrendingcardProduct"><img src="https://i.ibb.co/yq9Ggrw/Product1.png" alt="Product1" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1></div>
-                <div className="TrendingcardProduct"><img src="https://i.ibb.co/Fm0FPq7/Product2.png" alt="Product2" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1></div>
-                <div className="TrendingcardProduct"><img src="https://i.ibb.co/HT8GsvM/Product3.png" alt="Product3" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1></div>
-                <div className="TrendingcardProduct"><img src="https://i.ibb.co/Cb64F88/Product4.png" alt="Product4" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1></div>
-                <div className="TrendingcardProduct"><img src="https://i.ibb.co/yq9Ggrw/Product1.png" alt="Product1" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1></div>
-                <div className="TrendingcardProduct"><img src="https://i.ibb.co/Cb64F88/Product4.png" alt="Product4" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1></div>
+                <div className="TrendingcardProduct"><img src="https://i.ibb.co/yq9Ggrw/Product1.png" alt="Product1" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1><br></br></div>
+                <div className="TrendingcardProduct"><img src="https://i.ibb.co/Fm0FPq7/Product2.png" alt="Product2" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1><br></br></div>
+                <div className="TrendingcardProduct"><img src="https://i.ibb.co/HT8GsvM/Product3.png" alt="Product3" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1><br></br></div>
+                <div className="TrendingcardProduct"><img src="https://i.ibb.co/Cb64F88/Product4.png" alt="Product4" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1><br></br></div>
+                <div className="TrendingcardProduct"><img src="https://i.ibb.co/yq9Ggrw/Product1.png" alt="Product1" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1><br></br></div>
+                <div className="TrendingcardProduct"><img src="https://i.ibb.co/Cb64F88/Product4.png" alt="Product4" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1><br></br></div>
             </div>
-            <br></br>
-            <br></br>
         </>
     )
 }
