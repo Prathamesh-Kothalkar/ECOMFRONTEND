da60e0380823123d4a9eb5a26a3a8519b810e779
Varunp01 <152736714+Varunp01@users.noreply.github.com>
2024-08-20 12:45:16 +0530

    Update Trending.jsx

--- src/components/Trending/Trending.jsx
+++ src/components/Trending/Trending.jsx
@@ -2,7 +2,7 @@ import './Trending.css'
 export default function Trending(){
     return(
         <>
-        <div className='Title'><h1 className='heading1'>Top Sellers</h1></div><br></br>
+        <div className='Title'><h1 className='heading1 Title'>Top Sellers</h1></div><br></br>
             <div className="TrendingContainer">
                 <div className="TrendingcardProduct"><img src="https://i.ibb.co/yq9Ggrw/Product1.png" alt="Product1" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1></div>
                 <div className="TrendingcardProduct"><img src="https://i.ibb.co/Fm0FPq7/Product2.png" alt="Product2" /><h3 className='heading3'>₹ 993</h3><h1 className='heading1'>₹ 992</h1></div>
